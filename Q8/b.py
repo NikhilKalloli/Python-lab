298fa9a7a58a075ae5d5b6a2ebc0e25ef5117185
NikhilKalloli <1ms22cs091@msrit.edu>
2024-07-09 23:14:13 +0530

    Add files

--- Q8/b.py
+++ Q8/b.py
@@ -1,15 +1,15 @@
-file=open("file.txt", "w")
+file = open("file.txt", "w")
 for i in range(5):
-  text=input("Enter text: ")
-  file.write(text+"\n")
+  text = input("Enter text: ")
+  file.write(text + "\n")
 file.close()
 
 with open("file.txt", "r") as file:
-  text=file.read()
-  words=text.split()
-  Upper=0
-  lower=0
-  digits=0
+  text = file.read()
+  words = text.split()
+  Upper = 0
+  lower = 0 
+  digits = 0
   for word in words:
       for char in word:
           if char.isupper():
@@ -18,6 +18,7 @@ with open("file.txt", "r") as file:
               lower+=1
           elif char.isdigit():
               digits+=1
-  print("Number of uppercase letters:", Upper)
-  print("Number of lowercase letters:", lower)
-  print("Number of digits:", digits)
+
+  print("Number of uppercase letters: ", Upper)
+  print("Number of lowercase letters: ", lower)
+  print("Number of digits: ", digits)
--- Q9/a.py
+++ Q9/a.py
@@ -1,11 +1,3 @@
-# Develop a python program read a dataset and perform the following using 
-# Pandas  
-# • Visualize the dataset using plot (). 
-# • Draw the Scatter plot for the dataset on any column. 
-# • Display the scatter plot with different colors. 
-# • Draw the Histogram for the dataset on any column. 
-# 2 1,2,3,5 
-
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('Q9\iris.csv')
@@ -14,14 +6,19 @@ df = pd.read_csv('Q9\iris.csv')
 df.plot()
 plt.show()
 
-df.plot(kind="scatter",x='sepal.length',y='petal.length')
+df.plot(kind = "scatter", x = 'sepal.length', y = 'petal.length')
 plt.show()
 
-x,y=df['petal.length'],df['sepal.length']
+x,y = df['petal.length'], df['sepal.length']
 plt.scatter(x,y,c='red')
-x,y=df['petal.width'],df['sepal.width']
+
+x,y = df['petal.width'], df['sepal.width']
 plt.scatter(x,y,c='green')
+plt.xlabel('Petal')
+plt.ylabel('Sepal')
 plt.show()
 
 df['sepal.length'].plot(kind='hist')
+plt.xlabel("Sepal Length")
+plt.ylabel("Frequency")
 plt.show() 
